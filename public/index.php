d66440dc9adbe2b8e0589824e489aaf60b04893c
Leonhard Radonic <lradonic@pascom.net>
2024-07-04 17:05:36 +0200

    Added PHP-DI

--- public/index.php
+++ public/index.php
@@ -2,8 +2,24 @@
 
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
+use PhpNoFramework\HelloWorld;
+
+use function DI\create;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$obj = new PhpNoFramework\HelloWorld();
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAttributes(false);
+
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class),
+]);
+
+$container = $containerBuilder->build();
+
+$obj = $container->get(HelloWorld::class);
 
 $obj->announce();
